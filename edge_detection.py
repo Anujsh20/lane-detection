b5360b903a6f1f9d1d1de0a75d321fc8b721935f
Anuj <168450934+Anujsh20@users.noreply.github.com>
2026-07-29 21:36:45 +0200

    added extrapolation and lane draw function

--- edge_detection.py
+++ edge_detection.py
@@ -63,7 +63,6 @@ def draw_line(frame, line_coordinates):
     line_frame_cpy = frame.copy()
     for line in line_coordinates:
         x1, y1, x2, y2 = line
-        print(x1, y1, x2, y2)
         cv.line(line_frame_cpy, (x1, y1), (x2, y2), color=(0,0,255), thickness=3)
     
     cv.imwrite(output_path + "lined_frame.png", line_frame_cpy)
@@ -72,7 +71,65 @@ def draw_line(frame, line_coordinates):
     cv.destroyAllWindows()
     return line_frame_cpy
 
-draw_line(frame, detect_lines(apply_mask(get_edges(frame))))
+def extrapolation(line_coordinates):
+    left_lane = []
+    right_lane = []
+    left_slope = []
+    right_slope = []
+    left_intercept = []
+    right_intercept = []
+
+    for line in line_coordinates:
+        x1, y1, x2, y2 = line
+        #print(x1, y1, x2, y2)
+        slope = (y2 - y1)/(x2 - x1)
+        intercept = y1 - (slope * x1)
+
+        if slope <0 and abs(slope) > 0.5:
+            left_lane.append(line)
+            left_slope.append(slope)
+            left_intercept.append(intercept)
+        elif slope > 0 and abs(slope) > 0.5:
+            right_lane.append(line)
+            right_slope.append(slope)
+            right_intercept.append(intercept)
+    avg_left_slope = sum(left_slope)/len(left_slope)
+    avg_right_slope = sum(right_slope)/len(right_slope)
+    avg_left_intercept = sum(left_intercept)/len(left_intercept)
+    avg_right_intercept = sum(right_intercept)/len(right_intercept)
+
+
+    x_bottom_left = (540 - avg_left_intercept)/avg_left_slope
+    x_bottom_right = (540 - avg_right_intercept)/avg_right_slope
+    x_horizon_left = (270 - avg_left_intercept)/avg_left_slope
+    x_horizon_right = (270 - avg_right_intercept)/avg_right_slope
+
+
+
+
+
+    print(x_bottom_left)
+    print(x_bottom_right)
+    print(x_horizon_left)
+    print(x_horizon_right)
+
+    return x_bottom_left, x_bottom_right, x_horizon_left, x_horizon_right
+
+def draw_lane_lines(frame, x_bottom_left, x_bottom_right, x_horizon_right, x_horizon_left):
+    frame_cpy = frame.copy()
+    cv.line(frame_cpy, (int(x_bottom_left), 540), (int(x_horizon_left), 270), color=(0,0,255), thickness=3)
+    cv.line(frame_cpy, (int(x_bottom_right), 540), (int(x_horizon_right), 270), color=(0,0,255), thickness=3)
+
+    cv.imwrite(output_path + "lane_lines.png", frame_cpy)
+    cv.imshow("Lined_lanes_Frame", frame_cpy)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+
+x_bl, x_br, x_hl, x_hr = extrapolation(detect_lines(apply_mask(get_edges(frame))))
+draw_lane_lines(frame, x_bl, x_br, x_hr, x_hl)
+#draw_line(frame, detect_lines(apply_mask(get_edges(frame))))
 
 #detect_lines(apply_mask(get_edges(frame)))
 #get_edges(frame)
